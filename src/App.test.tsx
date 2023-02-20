1cabbf09353f4b207be96db21fc4142d6fe1a74f
David Johnston <djohnston@cydarm.com>
2023-02-20 14:21:58 +1100

    Everything

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+const MyPassword = () => {
+  return <label>Hello<input type="password" /></label>
+}
+
+const MyPassword2 = () => {
+  return <><label htmlFor="password">Hello</label><input type="password" id="password" /></>
+}
+const MyPassword3 = () => {
+  return <><label id="label">Hello</label><input type="password" aria-labelledby='label' /></>
+}
+
+// Passwords do not have the role textbox! 
+// https://www.w3.org/TR/html-aria/#:~:text=the%20spinbutton%20role.-,input%20type%3Dpassword,-No%20corresponding%20role
+// https://github.com/testing-library/dom-testing-library/issues/1128
+describe('Passwords', () => {
+  it("Passwords are selectedable label 1", () => {
+    render(<MyPassword />)
+
+    expect(screen.queryByRole("textbox", {
+      name: /hello/i
+    })).not.toBeInTheDocument();
+  });
+
+  it("Passwords are selectedable label 2", () => {
+    render(<MyPassword2 />)
+
+    expect(screen.queryByRole("textbox", {
+      name: /hello/i
+    })).not.toBeInTheDocument();
+  });
+
+  it("Passwords are selectedable label 3", () => {
+    render(<MyPassword3 />)
+
+    expect(screen.queryByRole("textbox", {
+      name: /hello/i
+    })).not.toBeInTheDocument();
+  });
 });
